6f47d6525f2e7428dc91a750d901f37a1a9cc1df
GreenCityLife <Greencitylife.sketchub@gmail.com>
2022-05-29 19:59:36 +0530

    Import and Export Theme is functional

--- app/src/main/java/com/greencitylife/greencoder/ThemeActivity.java
+++ app/src/main/java/com/greencitylife/greencoder/ThemeActivity.java
@@ -18,17 +18,29 @@ import android.animation.*;
 import android.view.animation.*;
 import java.util.*;
 import java.text.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import android.widget.ScrollView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ImageView;
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.content.Intent;
+import android.net.Uri;
+import android.content.ClipData;
 import android.view.View;
+import com.google.gson.Gson;
 import android.graphics.Typeface;
+import com.google.gson.reflect.TypeToken;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
 
 public class ThemeActivity extends AppCompatActivity {
 	
+	public final int REQ_CD_IMPORTER = 101;
 	
 	private Toolbar _toolbar;
 	private boolean is_expanded = false;
@@ -37,6 +49,10 @@ public class ThemeActivity extends AppCompatActivity {
 	private String sec_data = "";
 	private String desc_data = "";
 	private String high_data = "";
+	private String import_path = "";
+	private String import_data = "";
+	
+	private ArrayList<HashMap<String, Object>> export = new ArrayList<>();
 	
 	private ScrollView vscroll1;
 	private LinearLayout linear1;
@@ -45,6 +61,7 @@ public class ThemeActivity extends AppCompatActivity {
 	private LinearLayout main_color;
 	private LinearLayout second_color;
 	private LinearLayout desc_color;
+	private LinearLayout high_color;
 	private LinearLayout linear5;
 	private LinearLayout preview;
 	private LinearLayout set_theme;
@@ -58,6 +75,8 @@ public class ThemeActivity extends AppCompatActivity {
 	private TextView textview7;
 	private TextView textview8;
 	private TextView textview9;
+	private TextView textview17;
+	private TextView textview18;
 	private TextView textview13;
 	private ImageView imageview1;
 	private TextView textview14;
@@ -67,17 +86,29 @@ public class ThemeActivity extends AppCompatActivity {
 	private TextView textview10;
 	private TextView textview11;
 	private TextView textview12;
-	private LinearLayout high_color;
-	private TextView textview17;
-	private TextView textview18;
 	
 	private SharedPreferences file;
+	private Intent i = new Intent();
+	private Intent importer = new Intent(Intent.ACTION_GET_CONTENT);
 	@Override
 	protected void onCreate(Bundle _savedInstanceState) {
 		super.onCreate(_savedInstanceState);
 		setContentView(R.layout.theme);
 		initialize(_savedInstanceState);
-		initializeLogic();
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+		|| ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+			ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+		}
+		else {
+			initializeLogic();
+		}
+	}
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == 1000) {
+			initializeLogic();
+		}
 	}
 	
 	private void initialize(Bundle _savedInstanceState) {
@@ -99,6 +130,7 @@ public class ThemeActivity extends AppCompatActivity {
 		main_color = (LinearLayout) findViewById(R.id.main_color);
 		second_color = (LinearLayout) findViewById(R.id.second_color);
 		desc_color = (LinearLayout) findViewById(R.id.desc_color);
+		high_color = (LinearLayout) findViewById(R.id.high_color);
 		linear5 = (LinearLayout) findViewById(R.id.linear5);
 		preview = (LinearLayout) findViewById(R.id.preview);
 		set_theme = (LinearLayout) findViewById(R.id.set_theme);
@@ -112,6 +144,8 @@ public class ThemeActivity extends AppCompatActivity {
 		textview7 = (TextView) findViewById(R.id.textview7);
 		textview8 = (TextView) findViewById(R.id.textview8);
 		textview9 = (TextView) findViewById(R.id.textview9);
+		textview17 = (TextView) findViewById(R.id.textview17);
+		textview18 = (TextView) findViewById(R.id.textview18);
 		textview13 = (TextView) findViewById(R.id.textview13);
 		imageview1 = (ImageView) findViewById(R.id.imageview1);
 		textview14 = (TextView) findViewById(R.id.textview14);
@@ -121,10 +155,9 @@ public class ThemeActivity extends AppCompatActivity {
 		textview10 = (TextView) findViewById(R.id.textview10);
 		textview11 = (TextView) findViewById(R.id.textview11);
 		textview12 = (TextView) findViewById(R.id.textview12);
-		high_color = (LinearLayout) findViewById(R.id.high_color);
-		textview17 = (TextView) findViewById(R.id.textview17);
-		textview18 = (TextView) findViewById(R.id.textview18);
 		file = getSharedPreferences("file", Activity.MODE_PRIVATE);
+		importer.setType("application/json");
+		importer.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
 		
 		bc_color.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -154,6 +187,13 @@ public class ThemeActivity extends AppCompatActivity {
 			}
 		});
 		
+		high_color.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				_high_dialog();
+			}
+		});
+		
 		linear5.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View _view) {
@@ -173,7 +213,59 @@ public class ThemeActivity extends AppCompatActivity {
 		set_theme.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View _view) {
+				file.edit().putString("bc_color", bc_data).commit();
+				file.edit().putString("mn_color", main_data).commit();
+				file.edit().putString("sec_color", sec_data).commit();
+				file.edit().putString("desc_color", desc_data).commit();
+				file.edit().putString("hi_color", high_data).commit();
+				i.setAction(Intent.ACTION_VIEW);
+				i.setClass(getApplicationContext(), SettingsActivity.class);
+				startActivity(i);
+				finish();
+			}
+		});
+		
+		export_theme.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				{
+					HashMap<String, Object> _item = new HashMap<>();
+					_item.put("bc_color", bc_data);
+					export.add(_item);
+				}
 				
+				{
+					HashMap<String, Object> _item = new HashMap<>();
+					_item.put("main_color", main_data);
+					export.add(_item);
+				}
+				
+				{
+					HashMap<String, Object> _item = new HashMap<>();
+					_item.put("sec_color", sec_data);
+					export.add(_item);
+				}
+				
+				{
+					HashMap<String, Object> _item = new HashMap<>();
+					_item.put("desc_color", desc_data);
+					export.add(_item);
+				}
+				
+				{
+					HashMap<String, Object> _item = new HashMap<>();
+					_item.put("high_color", high_data);
+					export.add(_item);
+				}
+				
+				_export_dialog();
+			}
+		});
+		
+		import_theme.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				startActivityForResult(importer, REQ_CD_IMPORTER);
 			}
 		});
 		
@@ -181,13 +273,6 @@ public class ThemeActivity extends AppCompatActivity {
 			@Override
 			public void onClick(View _view) {
 				
-			}
-		});
-		
-		high_color.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				_high_dialog();
 			}
 		});
 	}
@@ -233,21 +318,7 @@ public class ThemeActivity extends AppCompatActivity {
 		setTitle("Themes");
 		is_expanded = false;
 		preview.setVisibility(View.GONE);
-		if (bc_data.equals("")) {
-			bc_data = "#212121";
-		}
-		if (main_data.equals("")) {
-			main_data = "#4CAF50";
-		}
-		if (sec_data.equals("")) {
-			sec_data = "#FFFFFF";
-		}
-		if (desc_data.equals("")) {
-			desc_data = "#9E9E9E";
-		}
-		if (high_data.equals("")) {
-			high_data = "#E0E0E0";
-		}
+		_reload();
 		setTheme(android.R.style.Theme_Material);
 	}
 	
@@ -256,7 +327,38 @@ public class ThemeActivity extends AppCompatActivity {
 		super.onActivityResult(_requestCode, _resultCode, _data);
 		
 		switch (_requestCode) {
-			
+			case REQ_CD_IMPORTER:
+			if (_resultCode == Activity.RESULT_OK) {
+				ArrayList<String> _filePath = new ArrayList<>();
+				if (_data != null) {
+					if (_data.getClipData() != null) {
+						for (int _index = 0; _index < _data.getClipData().getItemCount(); _index++) {
+							ClipData.Item _item = _data.getClipData().getItemAt(_index);
+							_filePath.add(FileUtil.convertUriToFilePath(getApplicationContext(), _item.getUri()));
+						}
+					}
+					else {
+						_filePath.add(FileUtil.convertUriToFilePath(getApplicationContext(), _data.getData()));
+					}
+				}
+				Uri uri = _data.getData();
+				import_path = uri.getPath();
+				import_path = import_path.replace("document", "storage");
+				import_path = import_path.replace(":", "/");
+				import_path = import_path.replace("primary", "emulated/0");
+				import_data = FileUtil.readFile(import_path);
+				export = new Gson().fromJson(import_data, new TypeToken<ArrayList<HashMap<String, Object>>>(){}.getType());
+				bc_data = export.get((int)0).get("bc_color").toString();
+				main_data = export.get((int)1).get("main_color").toString();
+				sec_data = export.get((int)2).get("sec_color").toString();
+				desc_data = export.get((int)3).get("desc_color").toString();
+				high_data = export.get((int)4).get("high_color").toString();
+				_reload();
+			}
+			else {
+				
+			}
+			break;
 			default:
 			break;
 		}
@@ -583,6 +685,90 @@ public class ThemeActivity extends AppCompatActivity {
 				high.dismiss();
 			} });
 		high.show();
+	}
+	
+	
+	private void _export_dialog () {
+		final AlertDialog ex = new AlertDialog.Builder(ThemeActivity.this).create();
+		View options = getLayoutInflater().inflate(R.layout.create_file,null); 
+		ex.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+		ex.setView(options);
+		TextView ok = (TextView) options.findViewById(R.id.textview3);
+		
+		TextView cancel = (TextView) options.findViewById(R.id.textview4);
+		
+		TextView title = (TextView) options.findViewById(R.id.textview1);
+		
+		TextView message = (TextView) options.findViewById(R.id.textview2);
+		
+		LinearLayout bg = (LinearLayout) options.findViewById(R.id.linear1);
+		
+		final LinearLayout linear6 = (LinearLayout) options.findViewById(R.id.linear6);
+		
+		final EditText input = (EditText) options.findViewById(R.id.edittext1);
+		input.setFocusable(true);
+		input.setFocusableInTouchMode(true);
+		title.setText("Enter the Name");
+		message.setText("Enter the name for the theme file (eg: Dark-Green)");
+		input.setHint("Enter Name");
+		ok.setText("Ok");
+		cancel.setText("Cancel");
+		title.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/en_medium.ttf"), 0);
+		message.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/en_medium.ttf"), 0);
+		ok.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/en_medium.ttf"), 0);
+		cancel.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/en_medium.ttf"), 0);
+		input.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/en_medium.ttf"), 0);
+		{
+			android.graphics.drawable.GradientDrawable SketchUi = new android.graphics.drawable.GradientDrawable();
+			int d = (int) getApplicationContext().getResources().getDisplayMetrics().density;
+			SketchUi.setColor(0xFF212121);
+			bg.setElevation(d*10);
+			bg.setBackground(SketchUi);
+		}
+		ok.setOnClickListener(new View.OnClickListener(){ public void onClick(View v){
+				if (!input.getText().toString().equals("")) {
+					ex.dismiss();
+					FileUtil.writeFile("/storage/emulated/0/.gncode/Themes/".concat(input.getText().toString().concat(".json")), new Gson().toJson(export));
+					SketchwareUtil.showMessage(getApplicationContext(), "Saved to storage/emulated/0/.gncode/Themes/".concat(input.getText().toString().concat(".json")));
+					export.clear();
+				} else {
+					input.setError("Empty Hex Code");
+				}
+			} });
+		cancel.setOnClickListener(new View.OnClickListener(){ public void onClick(View v){
+				ex.dismiss();
+			} });
+		ex.show();
+	}
+	
+	
+	private void _reload () {
+		if (bc_data.equals("")) {
+			bc_data = "#212121";
+		}
+		if (main_data.equals("")) {
+			main_data = "#4CAF50";
+		}
+		if (sec_data.equals("")) {
+			sec_data = "#FFFFFF";
+		}
+		if (desc_data.equals("")) {
+			desc_data = "#9E9E9E";
+		}
+		if (high_data.equals("")) {
+			high_data = "#E0E0E0";
+		}
+		preview.setBackgroundColor(Color.parseColor(bc_data));
+		preview_option.setBackgroundColor(Color.parseColor(bc_data));
+		textview3.setText(bc_data);
+		textview14.setTextColor(Color.parseColor(main_data));
+		textview5.setText(main_data);
+		textview15.setTextColor(Color.parseColor(sec_data));
+		textview7.setText(sec_data);
+		textview16.setTextColor(Color.parseColor(desc_data));
+		textview9.setText(desc_data);
+		_rippleRoundStroke(preview_option, bc_data, high_data, 0, 0, bc_data);
+		textview18.setText(high_data);
 	}
 	
 	
